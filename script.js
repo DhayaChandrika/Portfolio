fb04938269b2f23fd2e83be3ca15a3343416537d
DhayaChandrika <dhayachandrika@gmail.com>
2026-03-13 16:48:11 +0530

    files changed

---
+++ script.js
@@ -0,0 +1,11 @@
+// Scroll reveal
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((e) => {
+      if (e.isIntersecting) e.target.classList.add("in");
+    });
+  },
+  { threshold: 0.1 },
+);
+
+document.querySelectorAll(".reveal").forEach((el) => observer.observe(el));
